fb2c2a2b156d072bc62464b56e2d47fcebbdfe06
Michele Piccirillo <michele.piccirillo@gmail.com>
2011-11-30 00:04:11 +0100

    First commit

---
+++ AroundMe API/src/Test.java
@@ -0,0 +1,15 @@
+import it.unisannio.aroundme.middleware.Factory;
+import it.unisannio.aroundme.middleware.User;
+
+
+public class Test {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		User u = Factory.getInstance().newUser();
+	}
+
+}
