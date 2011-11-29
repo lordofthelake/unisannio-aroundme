a38e537ad08214e3d0f5f17972f69d59fe45d648
Michele Piccirillo <michele.piccirillo@gmail.com>
2011-11-30 00:14:03 +0100

    Test

--- AroundMe API/src/it/unisannio/aroundme/middleware/DataListener.java
+++ AroundMe API/src/it/unisannio/aroundme/middleware/DataListener.java
@@ -1,6 +1,11 @@
 package it.unisannio.aroundme.middleware;
 
-
+/**
+ * 
+ * @author m
+ *
+ * @param <T>
+ */
 public interface DataListener<T> {
 	void onData(T object);
 	void onError(Exception e);
